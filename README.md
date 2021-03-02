5e2f3974c74e9016972a40eda8546d5b61c6d28f
ColdeZhang <coldezhang@foxmail.com>
2021-03-02 15:25:45 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # Esp8266_Homekit_Template
 Template of Homekit for the Esp8266 based on Arduino
+
+基于Esp8266的Homekit固件编写方法
+
+## 入门知识：
+
+### 一、名词定义
+
+#### 1.配件（Accessories）
+
+##### 官方文档（2.3.1）解释：
+
+> Accessories are comprised of services and characteristics. An example of an accessory is a ceiling fan with a light and a mister that sprays cool water.
+
+并非是指硬件，而是一个概念（一个配件在HomeApp上就是一个开关之类的）。所以可以在同一个硬件上可以配置多个配件（比方带有温度检测功能的电风扇），但是除非有特殊需求（比方带有温度检测功能的电风扇），否则一般情况下不建议将许多配件揉杂在一个硬件上。
+
+### 2.服务（Services）
+
+##### 官方文档（2.3.2）解释：
+
+> Services group functionality in order to provide context. In the aforementioned example accessory there are three services: a fan service to interact with the ceiling fan, a light service to interact with the light, and a mister service to interact with the spray mister.
+
+服务定义了一个配件的实际功能，比方同样是属于开关（Switch）类别（Category）的配件，除了具有开关的基本（必须）功能，对于电风扇而言他可以具有调速的功能，对于电灯而言他可以有调整亮度或者颜色的功能。
+
+### 3.特征（Characteristics）
+
+##### 官方文档（2.3.3）解释：
+
+> A characteristic is a feature that represents data or an associated behavior of a service. The characteristic is defined by a universally unique type, and has additional properties that determine how the value of the characteristic can be accessed.
+
+特征在固件中也可以看作是变量，不同的服务要求有不同类型的特征（变量）。
+
+
+
+### 二、定义一个完整的配件
+
+一个完整的配件（Accessory）由若干个服务（Service）组成，其中必须包含一个“配件信息服务”（ACCESSORY_INFORMATION）与一个主服务（Primary Service），而主服务包含了若干的特征（Characteristic），并且分为必须（required）与可选（optional）。
+
+
+
+### 三、程序结构
+
+完整的固件程序由三个文件构成：
+
+#### 1.template.ino
+
+该文件为主程序，用于发送接收处理数据、操作IO口等等。
+
+#### 2.my_accessory.c
+
+该文件用于定义配件的属性。
+
+#### 3.wifi_info.h
+
+该文件专用于保存与处理wifi连接方法。
+
+
+
+## 模板使用方法：
+
+
+ * 使用此模板的方法：
+ * 1.确定你所需要的智能配件功能
+ * 2.前往 https://github.com/ColdeZhang/Esp8266_Homekit_Template/blob/main/HAP-Specification-Non-Commercial-Version.pdf 在p134查找对应的服务（service）
+ * 3.根据文档中服务（service）对变量（characteristic）的要求，创建变量（characteristic）
--- Template/my_accessory.c
+++ Template/my_accessory.c
@@ -2,6 +2,13 @@
  * my_accessory.c
  * 
  * 用于定义一个配件的类型、参数等信息
+ * 
+ * 在一个文件中可以定义多个配件（accessory）
+ * 其中每个配件（accessory）由若干个服务（service）组成
+ * 服务（service）则由对应的变量（characteristic）组成
+ * 
+ * 注意：
+ * 1.尽管此文件支持同时定义多个配件（accessory），但是为了便于管理与设置建议不要这么做
  */
 #include <homeKit-ESP8266/homekit.h>
 #include <HomeKit-ESP8266/characteristics.h>
@@ -11,5 +18,79 @@ void my_accessory_identify(homekit_value_t_value){
     printf("accessory identify\n");
 }
 
-//定义配件相关的变量 ｜ 变量名自定义 ｜ ｜ 变量类型可在 ｜
-homrkit_characteristic_t /*变量名*/ = HOMEKIT_CHARACTERISTIC_(/*变量类型*/,/*变量初始值*/);
+/*
+ * 定义配件相关的若干个变量
+ * ｜变量名｜
+ * 自定义一个变量的名字
+ * ｜变量类型｜
+ * 每个服务（service）都有若干种类型的变量，其中分为必须（required）和可选（optional）两种
+ * 具体的变量类型以及是否必须可在下面的链接中查看（p158）
+ * https://github.com/ColdeZhang/Esp8266_Homekit_Template/blob/main/HAP-Specification-Non-Commercial-Version.pdf
+ * ｜初始值｜
+ * 对应变量类型的说明赋予其一个初始值
+*/
+homrkit_characteristic_t /*变量A*/ = HOMEKIT_CHARACTERISTIC_(/*变量类型X*/,/*变量初始值*/);
+homrkit_characteristic_t /*变量B*/ = HOMEKIT_CHARACTERISTIC_(/*变量类型Y*/,/*变量初始值*/);
+homrkit_characteristic_t /*变量C*/ = HOMEKIT_CHARACTERISTIC_(/*变量类型Z*/,/*变量初始值*/);
+
+/*
+ * 定义配件们的属性
+ * 存储在accessories[]中
+*/
+homekit_accessory_t *accessories[] = {
+    /*
+     * 定义第一个配件的属性
+     * ｜编号｜从1开始，每个配件需要一个单独的编号
+     * ｜种类｜即配件所属的类别可在如下链接的文件中查看（p252）
+     * https://github.com/ColdeZhang/Esp8266_Homekit_Template/blob/main/HAP-Specification-Non-Commercial-Version.pdf
+     * 
+     * 注意：种类需要使用大写字母，空格使用下划线（_）代替
+    */
+    HOMEKIT_ACCESSORY(.id=/*编号*/, .category=/*种类*/, .services=(homekit_service_t*[]) {
+        /*
+         * 配件基本信息定义
+         * 从上到下依次为：
+         * ｜配件名称｜引号内可以随便写，但是同一个局域网内不能出现同样名字的配件
+         * ｜制造商名｜引号内可以随便写
+         * ｜序列号码｜引号内可以随便写
+         * ｜硬件型号｜引号内可以随便写
+         * ｜固件版本｜引号内可以随便写
+        */
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "未命名配件"),
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "Arduino HomeKit"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "0123456"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "ESP8266/ESP32"),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.0"),
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, my_accessory_identify),
+            NULL
+        }),
+        /*
+         * 定义配件的服务
+         * ｜服务名｜可在下面链接的文件中查看（p134）
+         * https://github.com/ColdeZhang/Esp8266_Homekit_Template/blob/main/HAP-Specification-Non-Commercial-Version.pdf
+         * ｜是否为主服务｜每个配件除信息服务外有且仅有一个主服务，如果是主服务则填写true，否则可不填（删除.primmary）
+         * 
+         * 注意：服务的变量名前需要加&
+        */
+        HOMEKIT_SERVICE(/*服务名*/, .primary=/*是否为主服务*/, .characteristics=(homekit_characteristic_t*[]) {
+            &/*变量A*/,
+			&/*变量B*/,
+            &/*变量C*/
+            NULL
+        }),
+        NULL
+    }),
+    NULL
+};
+
+/*
+ * 将上述设置打包成config方便在主函数中调用
+ * .password 为连接设备时需要输入的配对码，可随便填写
+ * 
+ * 注意：配对码必须为八位数
+*/
+homekit_server_config_t config = {
+		.accessories = accessories,
+		.password = "111-11-111"
+};
