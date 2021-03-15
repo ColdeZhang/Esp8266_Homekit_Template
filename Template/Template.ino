d3769de67b408f80629a6d21ab4580d6a73d950b
ColdeZhang <coldezhang@foxmail.com>
2021-03-15 18:16:10 +0800

    Update Template.ino

--- Template/Template.ino
+++ Template/Template.ino
@@ -7,13 +7,16 @@
  * 将你要写在setup()与loop()中的内容写在your_setup()与your_loop()内。
  * 除非您充分了解或者能理解此文件中已存在于setup()与loop()中的内容，否则请
  * 尽量不要修改或者改写他们中的内容
+ * 
+ * homekit_storage_reset();擦除配对记录，以便于重新配对
  */
 
 #include <Arduino.h>
 #include "wifi_info.h"
 #include <arduino_homekit_server.h>;
 
-#define LOG_D(fmt, ...)   printf_P(PSTR(fmt "\n") , ##__VA_ARGS__);
+//用于通过串口输出debug信息
+#define PRTLOG(fmt, ...)   printf_P(PSTR(fmt "\n") , ##__VA_ARGS__);
 
 /* 
  * 引入my_accessory.c中的配置与变量
@@ -37,6 +40,7 @@ void your_loop(){
 
 }
 
+
 //如果不了解请不要动这段代码
 void setup() {
     your_setup();
@@ -50,7 +54,7 @@ void loop() {
 	const uint32_t t = millis();
 	if (t > next_heap_millis) {
 		next_heap_millis = t + 5 * 1000;
-		LOG_D("Free heap: %d, HomeKit clients: %d",ESP.getFreeHeap(), arduino_homekit_connected_clients_count());
+		PRTLOG("Free heap: %d, HomeKit clients: %d",ESP.getFreeHeap(), arduino_homekit_connected_clients_count());
 	}
     your_loop();
     delay(10);
